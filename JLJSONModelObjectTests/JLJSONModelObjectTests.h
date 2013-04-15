ff696cf3d1ee4e9683709fc1bf0b4de05aa46c64
Joel Levin <joellevin.email@gmail.com>
2013-04-15 12:52:19 -0700

    Added the codez

---
+++ JLJSONModelObjectTests/JLJSONModelObjectTests.h
@@ -0,0 +1,13 @@
+//
+//  JLJSONModelObjectTests.h
+//  JLJSONModelObjectTests
+//
+//  Created by Joel Levin on 4/15/13.
+//  Copyright (c) 2013 Joel Levin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JLJSONModelObjectTests : SenTestCase
+
+@end
---
+++ JLJSONModelObjectTests/JLJSONModelObjectTests.m
@@ -0,0 +1,32 @@
+//
+//  JLJSONModelObjectTests.m
+//  JLJSONModelObjectTests
+//
+//  Created by Joel Levin on 4/15/13.
+//  Copyright (c) 2013 Joel Levin. All rights reserved.
+//
+
+#import "JLJSONModelObjectTests.h"
+
+@implementation JLJSONModelObjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JLJSONModelObjectTests");
+}
+
+@end
